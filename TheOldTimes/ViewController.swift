0f57de50f59c1acaa975359b772fe1164bb1ec3e
Ashik Chowdhury <chowdhury.ashik@gmail.com>
2019-04-08 19:52:59 -0400

    init

--- TheOldTimes/ViewController.swift
+++ TheOldTimes/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var papers = [[String:Any]] ()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let url = URL(string: "https://api.nytimes.com/svc/archive/v1/2019/1.json?api-key=olpx6Y2AeQIKAanvOmwukQgtiTXmCFgH")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            // This will run when the network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                
+                self.papers = dataDictionary["docs"] as! [[String:Any]]
+                print(self.papers)
+            }
+            
+            
+        }
     }
-
-
 }
 
