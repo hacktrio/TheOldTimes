f4b94335d9898db465d748cec72d8d375283d6ea
Rafael Carmilema <rafael_carmilema@glic.com>
2019-04-27 18:21:11 -0400

    Added searh bar in navigation bar in between 2 bar buttons

--- TheOldTimes/ViewController.swift
+++ TheOldTimes/ViewController.swift
@@ -10,11 +10,11 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchControllerDelegate, UISearchResultsUpdating, UISearchBarDelegate {
     
 //    var papers = [[String:Any]] ()
 //    let apiKey = "olpx6Y2AeQIKAanvOmwukQgtiTXmCFgH"
-    
+    var searchController : UISearchController!
     override func viewDidLoad() {
         super.viewDidLoad()
 //        let url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?&api-key="+apiKey
@@ -30,10 +30,28 @@ class ViewController: UIViewController {
     }
     
     func setupNavbar() {
-        let searchController = UISearchController(searchResultsController: nil)
-        navigationItem.searchController = searchController
-        navigationItem.hidesSearchBarWhenScrolling = false
         
+        self.searchController = UISearchController(searchResultsController:  nil)
+        
+        self.searchController.searchResultsUpdater = self
+        self.searchController.delegate = self
+        self.searchController.searchBar.delegate = self
+        
+        self.searchController.hidesNavigationBarDuringPresentation = false
+        self.searchController.dimsBackgroundDuringPresentation = true
+        
+        self.navigationItem.titleView = searchController.searchBar
+        
+        self.definesPresentationContext = true
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTapped))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(addTapped))
+        
+    }
+    @objc func addTapped() {
+        print("Buttons Tapped")
+    }
+    func updateSearchResults(for searchController: UISearchController) {
+        print("Test")
     }
 }
 
