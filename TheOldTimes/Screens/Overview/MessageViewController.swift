30800f669acd6da805e2eeb48e91d1235a62dbee
Ashik Chowdhury <chowdhury.ashik@gmail.com>
2019-05-12 00:09:02 -0400

    added message controller and chat controller

---
+++ TheOldTimes/Screens/Overview/MessageViewController.swift
@@ -0,0 +1,59 @@
+//
+//  MessageViewController.swift
+//  TheOldTimes
+//
+//  Created by Ashik Chowdhury on 5/4/19.
+//  Copyright © 2019 AshikChowdhury. All rights reserved.
+//
+
+import UIKit
+
+class MessageViewController: UIViewController {
+    
+    let composeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "plus"), for: .normal)
+        button.frame = CGRect(x: 160, y: 100, width: 50, height: 50)
+        button.layer.cornerRadius = 0.5 * button.bounds.size.width
+        button.layer.masksToBounds = true
+        button.tintColor = UIColor.init(red: 0/255, green: 128/255, blue: 128/255, alpha: 1)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tabBarController?.tabBar.isHidden = true
+        let image = UIImage(named: "messageicon")
+        let messagebutton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showChatLog))
+        self.navigationItem.rightBarButtonItem = messagebutton
+        
+        view.addSubview(composeButton)
+        
+        setupComposeButton()
+        
+    }
+    
+    func setupComposeButton() {
+        composeButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
+        composeButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -30).isActive = true
+        composeButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        composeButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
+    @objc func showChatLog() {
+        let chatLogController = ShowChatLogController(collectionViewLayout: UICollectionViewFlowLayout())
+        navigationController?.pushViewController(chatLogController, animated: true)
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
---
+++ TheOldTimes/Screens/Overview/ShowChatLogController.swift
@@ -0,0 +1,94 @@
+//
+//  ShowChatLogController.swift
+//  TheOldTimes
+//
+//  Created by Ashik Chowdhury on 5/4/19.
+//  Copyright © 2019 AshikChowdhury. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ShowChatLogController: UICollectionViewController, UITextFieldDelegate {
+    
+    var ref: DatabaseReference!
+    
+    lazy var inputTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter message..."
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.delegate = self
+        return textField
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tabBarController?.tabBar.isHidden = true
+        navigationItem.title = "Chat Log Controller"
+        collectionView.backgroundColor = UIColor.white
+        
+        setupInputComponents()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    func setupInputComponents() {
+        let containerView = UIView()
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(containerView)
+        containerView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        containerView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        containerView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        containerView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        let sendButton = UIButton(type: .system)
+        sendButton.setTitle("Send", for: .normal)
+        sendButton.translatesAutoresizingMaskIntoConstraints = false
+        sendButton.addTarget(self, action: #selector(handleSend), for: .touchUpInside)
+        containerView.addSubview(sendButton)
+        sendButton.rightAnchor.constraint(equalTo: containerView.rightAnchor).isActive = true
+        sendButton.centerYAnchor.constraint(equalTo: containerView.centerYAnchor).isActive = true
+        sendButton.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        sendButton.heightAnchor.constraint(equalTo: containerView.heightAnchor).isActive = true
+        
+        containerView.addSubview(inputTextField)
+        inputTextField.leftAnchor.constraint(equalTo: containerView.leftAnchor, constant: 10).isActive = true
+        inputTextField.centerYAnchor.constraint(equalTo: containerView.centerYAnchor).isActive = true
+        inputTextField.heightAnchor.constraint(equalTo: containerView.heightAnchor).isActive = true
+        inputTextField.rightAnchor.constraint(equalTo: sendButton.leftAnchor).isActive = true
+        
+        let separatorLine = UIView()
+        separatorLine.backgroundColor = UIColor.gray
+        separatorLine.translatesAutoresizingMaskIntoConstraints = false
+        containerView.addSubview(separatorLine)
+        separatorLine.leftAnchor.constraint(equalTo: containerView.leftAnchor).isActive = true
+        separatorLine.topAnchor.constraint(equalTo: containerView.topAnchor).isActive = true
+        separatorLine.widthAnchor.constraint(equalTo: containerView.widthAnchor).isActive = true
+        separatorLine.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        
+    }
+    
+    @objc func handleSend() {
+        let ref = Database.database().reference().child("messages")
+        let childRef = ref.childByAutoId()
+        let values = ["text": inputTextField.text!]
+        childRef.updateChildValues(values)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        handleSend()
+        return true
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
